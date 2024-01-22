37c7f330083f07b26efbcd22c9cc949f69f0564b
Daniel Boguszewski <315942@uwr.edu.pl>
2024-01-22 17:19:30 +0100

    Visual Studio setup
    Created VisualStudio project, configured basic client-server interactions and set up main page from tamplates

---
+++ asp.net-store-project.client/src/scripts/nav.js
@@ -0,0 +1,18 @@
+searchBar = document.querySelector(".search-bar");
+focusSearchBar = () => searchBar.classList.add("focused");
+unfocusSearchBar = () => searchBar.classList.remove("focused");
+
+searchBarInputArea = searchBar.querySelector(".search-bar-input-area");
+searchBarInputArea.addEventListener("focus", focusSearchBar);
+searchBarInputArea.addEventListener("focusout", unfocusSearchBar);
+
+menuArea = document.querySelector(".menu");
+openMenu = () => menuArea.style.top = "60px";
+hideMenu = () => menuArea.style.top = "7px";
+
+menuSection = document.querySelector(".menu-section");
+profileIcon = menuSection.querySelector("#profile-link");
+profileIcon.addEventListener("mouseover", openMenu);
+
+mainArea = document.querySelector("main");
+mainArea.addEventListener("mouseover", hideMenu);
