5df3353f29c79504d0dec3d15420e9b6135e894f
Daniel Boguszewski <315942@uwr.edu.pl>
2024-02-06 04:09:02 +0100

    Created controller for order summary

--- ASP.NET-store-project.Server/Controllers/BasketController.cs
+++ ASP.NET-store-project.Server/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
 using ASP.NET_store_project.Server.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Numerics;
+using System.Xml.Linq;
 
 namespace ASP.NET_store_project.Server.Controllers
 {
@@ -16,10 +19,27 @@ namespace ASP.NET_store_project.Server.Controllers
         private readonly ILogger<StoreController> _logger = logger;
 
         [HttpPost("/api/basket/summary")]
-        [Authorize(AuthenticationSchemes = "Bearer", Roles = nameof(RoleType.User))]
-        public string Summary()
+        //[Authorize(AuthenticationSchemes = "Bearer", Roles = nameof(RoleType.User))]
+        public IActionResult Summary()
         {
-            return "summary";
+            var customer = _context.Customers
+                .Where(customer => customer.UserName == "user"); // ommit authentication for now
+            var basket = customer
+                .SelectMany(customer => customer.Basket)
+                .Where(item => item.Order == null);
+
+            if (!basket.Any()) return BadRequest("Basket is empty");
+
+            int orderId = _context.Orders.Count() + 1;
+            var order = _context.Orders
+                .Add(new Order(orderId, customer.Single().UserName));
+            foreach (var item in basket)
+            {
+                item.OrderId = orderId;
+            }
+
+            _context.SaveChanges();
+            return Ok();
         }
 
         [HttpPost("/api/basket/item/add")]
