edd867661bae00d2ff7b8036caf4f17b33c53108
Daniel <315942@uwr.edu.pl>
2025-04-08 20:24:57 +0200

    Paginator is now functional

--- asp.net-store-project.client/src/components/Store/Settings/Paginator/Paginator.tsx
+++ asp.net-store-project.client/src/components/Store/Settings/Paginator/Paginator.tsx
@@ -1,39 +1,66 @@
+import { useRef } from 'react';
 import './Paginator.css'
 
 interface PaginatorProps {
-    pages: number;
-    selectedPageIndex: number;
+    pageCount: number;
+    pageIndex: number;
     handler(): void;
 }
 
-function Paginator({ selectedPageIndex, handler }: PaginatorProps) {
-    const focusPageIndex = (event: React.MouseEvent) => {
-        const pageIndex = (event.target as HTMLInputElement);
-        pageIndex.classList.remove("idle");
-        pageIndex.readOnly = false;
-        pageIndex.select();
-        pageIndex.focus();
+function Paginator({ pageCount, pageIndex, handler }: PaginatorProps) {
+    const pageIndexInput = useRef(null);
+    const pageIndexDisplay = useRef(null);
+
+    const focusPageIndex = () => {
+        const input = pageIndexInput.current! as HTMLInputElement;
+        input.classList.remove("hidden");
+        input.select();
+        input.focus();
     }
 
-    const unfocusPageIndex = (event: React.FocusEvent) => {
-        const pageIndex = (event.target as HTMLInputElement);
+    const unfocusPageIndex = () => {
+        const input = pageIndexInput.current! as HTMLInputElement;
+        input.classList.add("hidden");
+
         const onlyPositiveNumbersPattern = /^[1-9][0-9]*/;
-        if (!onlyPositiveNumbersPattern.test(pageIndex.value)) {
-            pageIndex.value = pageIndex.dataset.currentPage!;
-            handler();
-        } 
-        pageIndex.classList.add("idle");
-        pageIndex.readOnly = true;
+        if (!onlyPositiveNumbersPattern.test(input.value))
+            input.value = input.dataset.currentPage!;
+        else handler();
+    }
+
+    const unfocusOnEnter = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter')
+            unfocusPageIndex();
+    }
+
+    const moveUp = (event: React.MouseEvent) => {
+        event.preventDefault();
+        const input = pageIndexInput.current! as HTMLInputElement;
+        input.value = String(parseInt(input.value) + 1);
+        unfocusPageIndex();
+    }
+
+    const moveDown = (event: React.MouseEvent) => {
+        event.preventDefault();
+        const input = pageIndexInput.current! as HTMLInputElement;
+        input.value = String(parseInt(input.value) - 1);
+        unfocusPageIndex();
     }
 
     return <div className="setting-section">
         <div className="paginator">
-            <span className="page-changer">&#x25C2;</span>
-            <input type="text" className="page-index idle" 
-                onClick={focusPageIndex} onBlur={unfocusPageIndex} 
-                name="PageIndex" defaultValue={selectedPageIndex} 
-                data-current-page={selectedPageIndex} readOnly />
-            <span className="page-changer">&#x25B8;</span>
+            <button onClick={moveDown} className="page-changer">&#x25C2;</button>
+            <span className="page-selector">
+                <input ref={pageIndexInput} type="text" className="page-index hidden"
+                    onBlur={unfocusPageIndex} onKeyDown={unfocusOnEnter}
+                    name="PageIndex" defaultValue={pageIndex}
+                    data-current-page={pageIndex} />
+                <input ref={pageIndexDisplay} type="text" className="page-display"
+                    onClick={focusPageIndex}
+                    defaultValue={pageIndex + "/" + pageCount}
+                    data-current-page={pageIndex} readOnly />
+            </span>
+            <button onClick={moveUp} className="page-changer">&#x25B8;</button>
         </div>
     </div>;
 }
