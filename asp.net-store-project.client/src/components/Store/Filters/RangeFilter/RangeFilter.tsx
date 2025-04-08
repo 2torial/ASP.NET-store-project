09bcf1a22133aab2d50330a11b2148720f2800f8
Daniel <315942@uwr.edu.pl>
2025-04-08 20:46:50 +0200

    PriceRange input now looses focus after Enter

--- asp.net-store-project.client/src/components/Store/Filters/RangeFilter/RangeFilter.tsx
+++ asp.net-store-project.client/src/components/Store/Filters/RangeFilter/RangeFilter.tsx
@@ -10,15 +10,20 @@ function RangeFilter({ from, to }: RangeFilterProps) {
         (event.target as HTMLElement).parentElement!.classList.toggle("expanded");
     }
 
+    const unfocusOnEnter = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter')
+            (event.target as HTMLInputElement).blur();
+    }
+
     return <div className="filter-section drop-down-section expanded">
         <label className="drop-down-label" onClick={toggleSection} htmlFor="price-from">Price</label>
         <div className="range-setting drop-down-content">
             <span className="range-input">
-                <input type="text" id="price-from" name="PriceFrom" defaultValue={from} />
+                <input type="text" id="price-from" name="PriceFrom" defaultValue={from} onKeyDown={unfocusOnEnter} />
             </span>
             <span className="range-dash">&ndash;</span>
             <span className="range-input">
-                <input type="text" id="price-to" name="PriceTo" defaultValue={to} />$
+                <input type="text" id="price-to" name="PriceTo" defaultValue={to} onKeyDown={unfocusOnEnter} />$
             </span>
         </div>
     </div>;
