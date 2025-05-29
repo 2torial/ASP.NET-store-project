6c02359566809dec32d7dfe534927bd7943e4856
Daniel <315942@uwr.edu.pl>
2025-05-29 23:03:09 +0200

    ControllerBaseExtention identation fix

--- ASP.NET-store-project.Server/Extentions/ControllerBaseExtention.cs
+++ ASP.NET-store-project.Server/Extentions/ControllerBaseExtention.cs
@@ -5,13 +5,13 @@ namespace ASP.NET_store_project.Server.Extentions
     public static class ControllerBaseExtention
     {
         public static BadRequestObjectResult SingleErrorBadRequest(this ControllerBase controller, string name, string message) =>
-             controller.BadRequest(new { Errors = new Dictionary<string, string[]>() { { name, new string[] { message } } } });
+            controller.BadRequest(new { Errors = new Dictionary<string, string[]>() { { name, new string[] { message } } } });
 
         public static BadRequestObjectResult SingleErrorBadRequest(this ControllerBase controller, string name, string[] messages) =>
             controller.BadRequest(new { Errors = new Dictionary<string, string[]>() { { name, messages } } });
 
         public static BadRequestObjectResult SingleErrorBadRequest(this ControllerBase controller, string message) =>
-             controller.BadRequest(new { Errors = new Dictionary<string, string[]>() { { "other", new string[] { message } } } });
+            controller.BadRequest(new { Errors = new Dictionary<string, string[]>() { { "other", new string[] { message } } } });
 
         public static BadRequestObjectResult SingleErrorBadRequest(this ControllerBase controller, string[] messages) =>
             controller.BadRequest(new { Errors = new Dictionary<string, string[]>() { { "other", messages } } });
