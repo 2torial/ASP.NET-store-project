6fbebeccf24b30d83192d07901f088ee96d125dd
Daniel <315942@uwr.edu.pl>
2025-05-30 12:33:01 +0200

    Added comments to records meant for API communication

--- ASP.NET-store-project.Server/Models/StructuredData/ProductInfo.cs
+++ ASP.NET-store-project.Server/Models/StructuredData/ProductInfo.cs
@@ -2,6 +2,7 @@
 
 namespace ASP.NET_store_project.Server.Models.StructuredData
 {
+    // Uniform class for server-server and server-client communication
     public record ProductInfo(
         string? Id,
         string? BasketId,
@@ -16,6 +17,10 @@ namespace ASP.NET_store_project.Server.Models.StructuredData
     {
         public ProductInfo() : this(null, null, null, 0, 0, null, null, null, null, null) { }
 
+        // Creates a new instance of the same class with modified values
+        // Sets SupplierId and SupplierName according to supplier parameter
+        // Resets BasketId if basketId parameter is not null
+        // Recalculates Price if adjustPrice is set to true
         public ProductInfo NewModified(Supplier supplier, string? basketId = null, bool adjustPrice = true) => 
             new(Id, basketId ?? BasketId, Name, adjustPrice ? supplier.CalculateStorePrice(Price) : Price, Quantity, supplier.Id, supplier.Name, Thumbnail, Tags, PageContent);
     }
