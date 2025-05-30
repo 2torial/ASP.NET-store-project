fd0d540333a2e16798aebeb624490a32909311e7
Daniel <315942@uwr.edu.pl>
2025-05-30 12:34:19 +0200

    Added comments to classes representing React components

--- ASP.NET-store-project.Server/Models/ComponentData/OrderListComponentData.cs
+++ ASP.NET-store-project.Server/Models/ComponentData/OrderListComponentData.cs
@@ -2,5 +2,6 @@ using ASP.NET_store_project.Server.Models.StructuredData;
 
 namespace ASP.NET_store_project.Server.Models.ComponentData
 {
+    // Class representing React component
     public record OrderListComponentData(IEnumerable<OrderInfo> Orders);
 }
